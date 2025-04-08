b342066a38b5a2b7a57f16bfa4d94e3099868710
trtrantnt <tritrantnt@gmail.com>
2025-04-08 20:28:06 +0700

    Commit before branch rename

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin:'*'
 }))
 
-mongoose.connect("mongodb://localhost:27017/S6");
+mongoose.connect("mongodb://127.0.0.1:27017/S6");
 mongoose.connection.on('connected',()=>{
   console.log("connected");
 })
